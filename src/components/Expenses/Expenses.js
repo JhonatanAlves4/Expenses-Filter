b6d3028ac14c01eeca72fbfd08840b47f4005464
Jhonatan Alves <jhonatan4alves@gmail.com>
2022-04-03 00:32:32 -0300

    upload files

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
+import ExpensesChart from './ExpensesChart';
+
+const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+  }
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return(
+    <>
+    <Card className="expenses">
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+
+      <ExpensesChart expenses={filteredExpenses} />
+
+      {filteredExpenses.length === 0 && <p>No expenses found :/</p>}
+
+      {filteredExpenses.length > 0 &&  
+        filteredExpenses.map(expense => (
+          <ExpenseItem 
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date} />
+          ))
+      }
+
+    </Card>
+  </>
+  )
+}
+
+export default Expenses;
